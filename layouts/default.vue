c96c6e9657b11eee4b54868864478e059ea2cc72
Devireddy Pramod Reddy <devireddy.pramod@gmail.com>
2022-12-31 23:50:51 +0530

    test code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,64 +1,77 @@
 <template>
-    <v-card>
-        <v-layout>
-            <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+    <v-app id="inspire">
 
-            <v-app-bar color="primary" prominent>
-                <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar flat>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-                <v-toolbar-title>My files</v-toolbar-title>
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
 
-                <v-spacer></v-spacer>
+        <v-navigation-drawer v-model="drawer">
+            <!--  -->
+        </v-navigation-drawer>
 
-                <v-btn variant="text" icon="mdi-magnify"></v-btn>
-
-                <v-btn variant="text" icon="mdi-filter"></v-btn>
-
-                <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-            </v-app-bar>
-
-            <v-navigation-drawer v-model="drawer" bottom temporary>
-                <v-list :items="items"></v-list>
-            </v-navigation-drawer>
-
-            <v-main>
-                <v-container>
-                    <NuxtPage keep-alive />
-                </v-container>
-            </v-main>
-        </v-layout>
-    </v-card>
+        <v-main>
+            <NuxtPage></NuxtPage>
+        </v-main>
+    </v-app>
 </template>
   
 <script>
 export default {
-    data: () => ({
-        drawer: true,
-        group: null,
-        items: [
-            {
-                title: "Foo",
-                value: "foo",
-            },
-            {
-                title: "Bar",
-                value: "bar",
-            },
-            {
-                title: "Fizz",
-                value: "fizz",
-            },
-            {
-                title: "Buzz",
-                value: "buzz",
-            },
-        ],
-    }),
+    name: "DefaultLayout",
+    data () {
+        return {
+            drawer: true,
+            items: [
+                {
+                    icon: "mdi-chart-donut-variant",
+                    title: "Realtime",
+                    to: "/",
+                },
+                {
+                    icon: "mdi-history",
+                    title: "Offline",
+                    to: "/offline",
+                },
+            ],
+            title: "SATDEX",
+        };
+    },
 };
 </script>
-
 <style>
 html {
     overflow-y: auto;
+}
+
+.theme--light.v-navigation-drawer:not(.v-navigation-drawer--floating) .v-navigation-drawer__border {
+    background-color: transparent;
+}
+</style>
+
+<style scoped>
+.icon {
+    border-radius: 50%;
+    box-shadow: 0px 0px 0px 6px #752bdf;
+    width: 20px;
+    height: 20px;
+    margin-left: 35px;
+    margin-right: 15px;
+}
+
+.v-list-item {
+    align-items: center;
+    display: flex;
+    flex: 1 1 100%;
+    letter-spacing: normal;
+    min-height: 48px;
+    outline: none;
+    padding: 0 16px;
+    position: relative;
+    text-decoration: none;
+    border-radius: 8px;
+    margin: 10px 15px;
+    font-weight: 500;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
-
+        code here
+        <p>write whatever</p>
     </div>
 </template>
